4ca098b0a6ac9cf7cb8c15bccae649d7a5395512
Aryas1378 <aryas1378@gmail.com>
2020-01-03 19:22:57 +0330

    'user-part'

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path('submite/expense', views.submit_expense, name='submit_expense'),
-    path('submite/income', views.submit_income, name='submit_income')
-
+    path('submite/expense/', views.submit_expense, name='submit_expense'),
+    path('submite/income/', views.submit_income, name='submit_income'),
+    path('register/', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('logout', views.logout, name='logout')
 ]
